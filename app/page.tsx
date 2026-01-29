2895518a00c573c8ab77c6ff411a64a3b0c7036a
Ben Hicks <dev-trial@marketingsweet.co>
2026-01-29 13:19:35 +1030

    init

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen gap-4'>
+      <h1 className='text-4xl font-bold'>Welcome to the Anderson Trial Task</h1>
+      <p className='text-lg italic mt-6'>All instructions can be found in the README.md file</p>
+      <p className='text-lg italic'>All assets have been provided in the public folder</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ data/products.ts
@@ -0,0 +1,35 @@
+interface Product {
+  id: number;
+  name: string;
+  monthlyPrice: number;
+  totalMonthlyPrice: number;
+  totalMonthlySavings: number;
+  totalAnnualPrice: number;
+}
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: 'Bronze Membership',
+    monthlyPrice: 140,
+    totalMonthlyPrice: 1680,
+    totalMonthlySavings: 420,
+    totalAnnualPrice: 2140,
+  },
+  {
+    id: 2,
+    name: 'Silver Membership',
+    monthlyPrice: 240,
+    totalMonthlyPrice: 2880,
+    totalMonthlySavings: 3420,
+    totalAnnualPrice: 540,
+  },
+  {
+    id: 3,
+    name: 'Gold Membership',
+    monthlyPrice: 340,
+    totalMonthlyPrice: 3680,
+    totalMonthlySavings: 820,
+    totalAnnualPrice: 6820,
+  },
+];
