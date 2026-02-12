408d96976af24214f624a28be8a9fb25d5903984
Marsh-ops <mh_rossi@hotmail.com>
2026-02-12 19:49:23 +1030

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
+'use client';
+
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer"
@@ -18,11 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`antialiased`}>
-        {/* <CartProvider>
+        <CartProvider>
         <Header />
           {children}  
         <Footer />
-        </CartProvider> */}
+        </CartProvider>
       </body>
     </html>
   );
